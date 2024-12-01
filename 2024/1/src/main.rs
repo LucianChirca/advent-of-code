b2c494a534325239132e2c9b3e936b03c7e7d275
Lucian Chirca <lucian@elnino.tech>
2024-12-01 12:54:46 +0100

    Solve part two

--- 2024/1/src/main.rs
+++ 2024/1/src/main.rs
@@ -3,5 +3,5 @@ mod part_two;
 mod utils;
 
 fn main() {
-    println!("Part one result: {}", part_one::solve());
+    println!("Part two result: {}", part_two::solve());
 }
--- 2024/1/src/part_two.rs
+++ 2024/1/src/part_two.rs
@@ -1,8 +1,17 @@
+use std::collections::HashMap;
 use crate::utils::read_input;
 
 pub fn solve() -> i32 {
-    let (mut list_one, mut list_two) = read_input();
+    let (list_one, list_two) = read_input();
+
+    let mut counts: HashMap<i32, i32> = HashMap::new();
+    for &num in &list_two {
+        *counts.entry(num).or_insert(0) += 1;
+    }
     
-    // TODO: Implement
-    return 0;
+    return list_one.iter()
+                    .map(
+                        |num|
+                        num * (*counts.get(num).unwrap_or(&0))
+                    ).sum();
 }
